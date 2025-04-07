294019139fa25b2f47a01d0e027a5dea7414a4e5
Nail <nailbest001@gmail.com>
2025-04-07 20:48:18 +0500

    new drawable and starting main.activity

--- app/src/main/java/com/example/lab09/MainActivity.java
+++ app/src/main/java/com/example/lab09/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.lab09;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    private static final String TAG = "MainActivity";
+
+    private static final String KEY_IS_SERVICE_RUNNING = "is_service_running";
+    private static final String KEY_LAST_CHARACTER = "last_character";
+
+    private TextView randomCharacterTextView;
+    private TextView statusTextView;
+    private Button startButton;
+    private Button stopButton;
 }
